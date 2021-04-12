51cca1a311125dd1700e1964ed6630b868a45613
etycomputer-github <etycomputer@gmail.com>
2021-04-13 03:59:05 +1200

    ✅ Adding unit test for the postgres class

--- tests/app_factory.py
+++ tests/app_factory.py
@@ -1,7 +1,13 @@
-from app import producer
+from app import producer, database
 from app.producer import Producer
+from app.database import MyPostgresDB
 
 
 def build_production_app() -> Producer:
     app = producer.create_producer()
     return app
+
+
+def build_postgres_db(test_mode=True) -> MyPostgresDB:
+    db = database.create_producer(test_mode)
+    return db
--- tests/test_producer.py
+++ tests/test_producer.py
@@ -2,7 +2,7 @@ import unittest
 from tests import app_factory
 
 
-class MyTest(unittest.TestCase):
+class MyProducerTest(unittest.TestCase):
     app = None
 
     def setUp(self):
