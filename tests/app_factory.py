bba8bbf5df93b7f76e866f97ef026ae487289f1e
etycomputer-github <etycomputer@gmail.com>
2021-04-14 02:57:33 +1200

    ✅ Adding integration unit tests for Kafka producer and consumer

--- tests/app_factory.py
+++ tests/app_factory.py
@@ -1,6 +1,7 @@
-from app import producer, database
+from app import producer, database, kafka
 from app.producer import Producer
 from app.database import MyPostgresDB
+from app.kafka import MyKafkaProducer, MyKafkaConsumer
 
 
 def build_production_app() -> Producer:
@@ -9,5 +10,15 @@ def build_production_app() -> Producer:
 
 
 def build_postgres_db(test_mode=True) -> MyPostgresDB:
-    db = database.create_producer(test_mode)
+    db = database.create_postgres_db(test_mode)
     return db
+
+
+def build_kafka_producer(test_mode=True) -> MyKafkaProducer:
+    kafka_p = kafka.create_kafka_producer(test_mode)
+    return kafka_p
+
+
+def build_kafka_consumer(test_mode=True) -> MyKafkaConsumer:
+    kafka_c = kafka.create_kafka_consumer(test_mode)
+    return kafka_c
