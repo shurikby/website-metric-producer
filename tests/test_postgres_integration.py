d8b7b470b9cff1a6a5b23c30d240aebf1b709280
etycomputer-github <etycomputer@gmail.com>
2021-04-13 13:21:06 +1200

    ✅ Added postgres integration tests for getting the list of urls

--- tests/test_postgres_integration.py
+++ tests/test_postgres_integration.py
@@ -28,6 +28,20 @@ class MyPostgresTest(unittest.TestCase):
         self.assertTrue(self.my_db.seed_db())
         self.assertTrue(self.my_db.drop_db())
 
+    def test_get_urls(self):
+        if integration_mode is False:
+            self.assertTrue(True)
+            return
+        self.assertTrue(self.my_db.drop_db())
+        self.assertTrue(self.my_db.create_db())
+        self.assertTrue(self.my_db.seed_db())
+        url_result = self.my_db.get_urls()
+        self.assertEqual(2, len(url_result))
+        for url in url_result:
+            self.assertEqual(4, len(url_result[url]))
+            self.assertEqual({'url_id', 'sample_frequency_s', 'url_path', 'regex_pattern'}, url_result[url].keys())
+        self.assertTrue(self.my_db.drop_db())
+
 
 if __name__ == '__main__':
     unittest.main()
