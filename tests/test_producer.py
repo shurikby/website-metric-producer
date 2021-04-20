698445532512bd55468d9267ce130147b520f979
etycomputer-github <etycomputer@gmail.com>
2021-04-20 22:19:08 +1200

    ✅ Added get_website_metrics tests for targeting valid and invalid urls

--- tests/test_producer.py
+++ tests/test_producer.py
@@ -1,6 +1,8 @@
 import unittest
+
+from app.producer import get_website_metrics
 from tests import app_factory
-from config import integration_mode
+from config import integration_mode, target_website_simulator_url
 
 
 class MyProducerTest(unittest.TestCase):
@@ -27,6 +29,17 @@ class MyProducerTest(unittest.TestCase):
             self.assertTrue(True)
             return
         self.assertTrue(isinstance(self.app, app_factory.Producer))
+
+    def test_get_website_metrics(self):
+        if integration_mode is False:
+            self.assertTrue(True)
+            return
+        sample_time, status_code, elapsed_seconds, isFound = get_website_metrics('http://127.0.0.1:1234/')
+        self.assertEqual(503, status_code)
+        self.assertFalse(isFound)
+        sample_time, status_code, elapsed_seconds, isFound = get_website_metrics(target_website_simulator_url)
+        self.assertEqual(200, status_code)
+        self.assertTrue(isFound)
 
 
 if __name__ == '__main__':
